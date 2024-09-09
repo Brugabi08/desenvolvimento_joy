dec7fd8a786717c33f6f9a79eb22eb8c14f9e7c2
Brugabi08 <bruna.gabri08@gmail.com>
2024-09-09 19:32:36 -0300

    atualizações

---
+++ addEventListener/script.js
@@ -0,0 +1,32 @@
+// let numero = document.querySelector("h1");
+// let botao = document.querySelector("button");
+// let contagem = 0;
+// function adicionarUm(){
+//     contagem = contagem + 1;
+//     numero.innerText = contagem;
+// }
+
+// botao.addEventListener("click", adicionarUm);
+
+let button = document.querySelector("button");
+let span = document.querySelector("span");
+let section = document.querySelector("section");
+function mostrarSpan(){
+    span.style.opacity = "100"
+}
+button.addEventListener("mouseover", mostrarSpan);
+
+function ocultarSpan(){
+    span.style.opacity = "0"
+}
+button.addEventListener("mouseout", ocultarSpan);
+
+// function fazerUmClick(){
+//     section.innerText = "Fez um click simples!"
+// }
+// button.addEventListener("click", fazerUmClick);
+
+function fazerDoisClicks(){
+    section.innerText = "Fez um duplo click!"
+}
+button.addEventListener("dblclick", fazerDoisClicks);
